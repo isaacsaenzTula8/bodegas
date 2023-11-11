80a5c15e854ef8debbc0375881c019779854dce0
Isaac Saenz <isaac@tulasalud.org>
2023-11-10 21:38:08 -0600

    se mejoró la interfaz de productos
    SE trabajara la misma interfaz para todos los formularios

--- create_producto.php
+++ create_producto.php
@@ -1,28 +1,36 @@
 <?php 
 include 'menuPrincipal.php';
 ?>
-
+  <div class=" text-center mt-5 ">
+    <h1 >Productos</h1>
+  </div>
 <div class="container min-vh-100 d-flex justify-content-center align-items-center" style="background-color:#E6E6FA">
-<form action = "procesar_creacion_producto.php" >
-  <div class="mb-3">
-    <label class="form-label">Código:</label>
-    <input type="text" class="form-control" id="codigoProd" name="codigo">
-  </div>
+  <div class="card mt-2 mx-auto p-4 bg-light">
+    <div class="card-body bg-light">
+      <form action = "procesar_creacion_producto.php" >
+        
+        <div class="mb-3">
+          <label class="form-label">Código:</label>
+          <input type="text" class="form-control" id="codigoProd" name="codigo">
+        </div>
 
-  <div class="mb-3">
-    <label class="form-label">Descripción:</label>
-    <input type="text" class="form-control" id="descripcionProd" name="descripcion">
-  </div>
+        <div class="mb-3">
+          <label class="form-label">Descripción:</label>
+          <input type="text" class="form-control" id="descripcionProd" name="descripcion">
+        </div>
  
   <!-- este debería de cambiarse por un select y realizar un catalogo de unidades de medida-->
-    <label class="form-label">Unidad de Medida:</label>
-    <input type="text" class="form-control" id="unidadMedida" name="unidad_medida">
+        <label class="form-label">Unidad de Medida:</label>
+        <input type="text" class="form-control" id="unidadMedida" name="unidad_medida">
  
     <div class="mb-3">
     <label class="form-label">Precio:</label>
-    <input type="text" class="form-control" id="codigoProd" name="precio">
+    <input type="text" class="form-control" id="precioProd" name="precio">
   </div>
  
- <button type="submit" class="btn btn-primary">Guardar</button>
+ <button type="submit" class="btn btn-primary btn-send">Guardar</button>
 </form>
+</span>
+    </div>
+  </div>
 </div>
--- header.php
+++ header.php
@@ -8,6 +8,7 @@ y no tener que escribirlo en cada uno de ellos. -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
